d28fbd764235e22f5f26645d9d5ac8a3c6f19c32
Poojabafnaprakash <poojabafnaprakash@gmail.com>
2016-09-13 00:10:53 -0700

    Reverse LL

---
+++ ReverseLL.java
@@ -0,0 +1,25 @@
+/* rse a linked list and return pointer to the head
+  The input list will have at least one element  
+  Node is defined as  
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+    // This is a "method-only" submission. 
+    // You only need to complete this method. 
+Node Reverse(Node head) {
+    Node curr = head;
+    Node prev = null, next;
+    if(head == null){
+        return null;
+    }
+    while(curr != null){
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+
+}

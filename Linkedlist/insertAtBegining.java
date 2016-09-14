3e5d8c60aa86bb2907a64dc0ae76caedce42c90e
Poojabafnaprakash <poojabafnaprakash@gmail.com>
2016-09-13 21:31:02 -0700

    insert at the begining

---
+++ Linkedlist/insertAtBegining.java
@@ -0,0 +1,28 @@
+/*
+  Insert Node at the beginning of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+// This is a "method-only" submission. 
+// You only need to complete this method. 
+
+Node Insert(Node head,int x) {
+    Node node;
+    if(head == null){
+        node = new Node();
+        node.data = x;
+        node.next = null;
+        return node;
+    }
+    else{
+        node = new Node();
+        node.next = head;
+        node.data = x;
+        head = node;
+        return head;
+    }
+}
